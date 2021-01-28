b3ca4ac867cebc7bb9ffeefef35df59621dc7824
prim <prim1957@gmail.com>
2021-01-28 20:34:59 +0200

    HomeWork 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-      </header>
-    </div>
+    <>
+      <Header 
+        title="This is title" 
+        descr="This is Description!"
+      />
+      <Layout
+        urlBg="./assets/bg2.jpg"
+      />
+      <Layout
+        colorBg="#00ff00"
+      />
+      <Layout
+        urlBg="./assets/bg3.jpg"
+      />
+      <Footer />
+    </>
   );
 }
+
 
 export default App;
